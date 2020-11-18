ef679b6f49a558fd269fb82b7e702f9f8d391292
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2020-11-18 23:46:05 +0900

    Add files via upload

---
+++ logincheck2.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost","root","branson113","mydb") or die("mysql connect error");
+$query= "select * from mydb where id= '$id' and pwd='$pwd';";
+$result= mysqli_query($conn,$query) or die("query error");
+$row = mysqli_fetch_array($result)
+//$queryid= "select id from mydb where id= '{$_POST['id']}';";
+//$resultid= mysqli_query($conn,$queryid) or die("query error");
+//$querypwd= "select pwd from mydb where pwd= '{$_POST['pwd']}';";
+//$resultpwd= mysqli_query($conn,$querypwd) or die("query error");
+
+if (($row['id'] ==$id]) && ($row['pwd'] ==$pwd))
+{
+  $_SESSION['id']=$row['id'];
+  $_SESSION['pwd']=$row['pwd'];
+  header("location: index2.php");
+}
+else{
+  echo "잘못된 정보입니다.";
+  //echo "<input type="button" name="success" value="OK" onclick="location.href='login.php'">";
+}
+mysqli_close($conn);
+ ?>
